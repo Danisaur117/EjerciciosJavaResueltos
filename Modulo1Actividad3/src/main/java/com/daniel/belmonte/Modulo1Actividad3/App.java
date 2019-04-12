a64a06b9b6ce6c888b57ccafb5ea8fbb9c2886d2
Danisaur117 <danisaur@gmail.com>
2019-04-12 16:59:46 +0200

    Module 1 - Activity 3 started but not implemented

---
+++ Modulo1Actividad3/src/main/java/com/daniel/belmonte/Modulo1Actividad3/App.java
@@ -0,0 +1,21 @@
+package com.daniel.belmonte.Modulo1Actividad3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * MÓDULO 1 - ACTIVIDAD 3
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	for(int i = 0; i < 10; i++) {
+    		System.out.println("Number " + i + "? ");
+    		
+    	}
+    }
+}
