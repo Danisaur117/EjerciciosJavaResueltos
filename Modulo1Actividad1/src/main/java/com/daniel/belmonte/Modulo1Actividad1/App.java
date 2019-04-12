0a21380547a4f1277d9279998f6a785562f0c1bd
Danisaur117 <danisaur@gmail.com>
2019-04-12 16:32:18 +0200

    Module 1 - Activity 1 implemented

---
+++ Modulo1Actividad1/src/main/java/com/daniel/belmonte/Modulo1Actividad1/App.java
@@ -0,0 +1,35 @@
+package com.daniel.belmonte.Modulo1Actividad1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String year = "";
+    	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    	System.out.println("Year? ");
+
+    	try {
+    		year = reader.readLine();
+    	}
+    	catch(Exception e) {
+    		System.out.println(e);
+    	}
+
+    	if(isLeap(year)) {
+    		System.out.println("Year " + year + " IS leap");
+    	}
+    	else {
+    		System.out.println("Year " + year + " ISN'T leap");
+    	}
+    }
+    
+    public static boolean isLeap(String year) {
+    	if(Integer.parseInt(year) % 4 == 0) return true;
+    	
+    	return false;
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## MÓDULO 1
 
-- [ ] Actividad 1
+- [x] Actividad 1
 - [ ] Actividad 2
 - [ ] Actividad 3
 - [ ] Actividad 4
