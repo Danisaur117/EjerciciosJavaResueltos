982670b6406e28ffb75fd9771a765affb171f9fe
Danisaur117 <danisaur@gmail.com>
2019-04-12 16:05:17 +0200

    Added HolaMundo project

---
+++ HolaMundo/src/main/java/com/daniel/belmonte/HolaMundo/App.java
@@ -0,0 +1,13 @@
+package com.daniel.belmonte.HolaMundo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("¡Hola Mundo! Esto es un Proyecto de Maven creado desde Eclipse");
+    }
+}
