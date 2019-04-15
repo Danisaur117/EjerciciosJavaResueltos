94a4cb40a6f8a54c76a61b3b2c56b76f93ba3515
Danisaur117 <danisaur@gmail.com>
2019-04-15 19:13:14 +0200

    Small modifications added to Activity 3

--- Modulo1Actividad3/src/main/java/com/daniel/belmonte/Modulo1Actividad3/App.java
+++ Modulo1Actividad3/src/main/java/com/daniel/belmonte/Modulo1Actividad3/App.java
@@ -2,6 +2,7 @@ package com.daniel.belmonte.Modulo1Actividad3;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /**
  * MÓDULO 1 - ACTIVIDAD 3
@@ -12,10 +13,23 @@ public class App
     public static void main( String[] args )
     {
     	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    	ArrayList<String> lista = new ArrayList<String>();
     	
     	for(int i = 0; i < 10; i++) {
     		System.out.println("Number " + i + "? ");
     		
+    		try {
+    			lista.add(reader.readLine());
+        	}
+        	catch(Exception e) {
+        		System.out.println(e);
+        	}
+    	}
+    	
+    	lista.sort(null);
+    	
+    	for(int j = 0; j < lista.size(); j++) {
+    		System.out.println(lista.get(j));
     	}
     }
 }
