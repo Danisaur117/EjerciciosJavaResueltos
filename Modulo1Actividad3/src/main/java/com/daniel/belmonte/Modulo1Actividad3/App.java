4fd0be422ebc4f5b0487adeae749234e824d998e
Danisaur117 <danisaur@gmail.com>
2019-04-16 14:07:26 +0200

    Module 1 - Activity 3 implemented and working OK

--- Modulo1Actividad3/src/main/java/com/daniel/belmonte/Modulo1Actividad3/App.java
+++ Modulo1Actividad3/src/main/java/com/daniel/belmonte/Modulo1Actividad3/App.java
@@ -3,6 +3,8 @@ package com.daniel.belmonte.Modulo1Actividad3;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * MÓDULO 1 - ACTIVIDAD 3
@@ -13,23 +15,34 @@ public class App
     public static void main( String[] args )
     {
     	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    	ArrayList<String> lista = new ArrayList<String>();
+    	ArrayList<Double> lista = new ArrayList<Double>();
+    	String numero = "";
     	
     	for(int i = 0; i < 10; i++) {
-    		System.out.println("Number " + i + "? ");
-    		
     		try {
-    			lista.add(reader.readLine());
+    			do {
+    				System.out.println("[" + i + "] Introduce un número: ");
+    				numero = reader.readLine();
+    			}
+    			while(!numero.matches("^[-]?[0-9]+((\\.)\\d{0,10})?$"));
+    			
+    			lista.add(Double.valueOf(numero));
         	}
         	catch(Exception e) {
         		System.out.println(e);
         	}
     	}
     	
-    	lista.sort(null);
+//    	System.out.println("Hay " + lista.size() + " elementos numéricos leídos");
+    	
+    	Collections.sort(lista, new Comparator<Double>(){
+    		public int compare(Double d1, Double d2) {
+    			return d1.compareTo(d2);
+    		}
+    	});
     	
     	for(int j = 0; j < lista.size(); j++) {
-    		System.out.println(lista.get(j));
+    		System.out.println(lista.get(j).doubleValue());
     	}
     }
 }
