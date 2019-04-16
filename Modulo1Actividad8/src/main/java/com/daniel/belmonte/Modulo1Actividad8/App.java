148a1804d4854b2edc76b3ea72b5d14dda35873c
Danisaur117 <danisaur@gmail.com>
2019-04-16 17:46:53 +0200

    Module 1 - Activity 8 started but not implemented yet

---
+++ Modulo1Actividad8/src/main/java/com/daniel/belmonte/Modulo1Actividad8/App.java
@@ -0,0 +1,35 @@
+package com.daniel.belmonte.Modulo1Actividad8;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * MÓDULO 1 - ACTIVIDAD 8
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    	String leido = "";
+    }
+    
+    public static double getFibbonacci(int startPoint) {    	
+//    	if (n>1){
+//    	       return fibonacci(n-1) + fibonacci(n-2);  //función recursiva
+//    	    }
+//    	    else if (n==1) {  // caso base
+//    	        return 1;
+//    	    }
+//    	    else if (n==0){  // caso base
+//    	        return 0;
+//    	    }
+//    	    else{ //error
+//    	        System.out.println("Debes ingresar un tamaño mayor o igual a 1");
+//    	        return -1; 
+//    	    }
+    	
+    	return 0.0;
+    }
+}
