3083a9779fdcb8dd3454531b449ac76e6a682b1f
Danisaur117 <danisaur@gmail.com>
2019-04-22 21:22:30 +0200

    Module 2 - Activity 1 source files added

---
+++ Modulo2Actividad1/src/main/java/com/daniel/belmonte/Modulo2Actividad1/App.java
@@ -0,0 +1,20 @@
+package com.daniel.belmonte.Modulo2Actividad1;
+
+/**
+ * MÓDULO 2 - ACTIVIDAD 1
+ * 
+ * Crea un BinaryArray, este BinaryArray es la representación privada de una matriz de variables
+ * booleanas. Por ejemplo la representación del BinaryArray "FTTF" sería una matriz de longitud
+ * cuatro que almacenaría (false, true, true, false) en los índices 0, 1, 2 y 3 de la matriz,
+ * respectivamente. La clase BinaryArray tiene la sisguiente funcionalidad:
+ * 		- Un constructor de un único parámetro que contiene un objeto String
+ * 		- Métodos accesorios get/set para acceder a una variable en un índice concreto o modificarla
+ * 
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Modulo2Actividad1/src/main/java/com/daniel/belmonte/Modulo2Actividad1/BinaryArray.java
@@ -0,0 +1,17 @@
+package com.daniel.belmonte.Modulo2Actividad1;
+
+public class BinaryArray {
+	private String arr;
+	
+	public BinaryArray(String arr) {
+		this.arr = arr;
+	}
+	
+	public String getArr() {
+		return arr;
+	}
+	
+	public void setArr(String arr) {
+		this.arr = arr;
+	}
+}
