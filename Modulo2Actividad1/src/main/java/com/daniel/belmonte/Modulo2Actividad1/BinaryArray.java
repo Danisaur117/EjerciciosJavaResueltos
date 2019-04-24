66979dd18b07ac6b2d45d9d6c8c2a7e982c8a417
Danisaur117 <danisaur@gmail.com>
2019-04-24 17:45:54 +0200

    Modified Binary Array class

--- Modulo2Actividad1/src/main/java/com/daniel/belmonte/Modulo2Actividad1/BinaryArray.java
+++ Modulo2Actividad1/src/main/java/com/daniel/belmonte/Modulo2Actividad1/BinaryArray.java
@@ -1,17 +1,37 @@
 package com.daniel.belmonte.Modulo2Actividad1;
 
 public class BinaryArray {
-	private String arr;
+	private Boolean[] arr;
+	private boolean hasNulls;
 	
-	public BinaryArray(String arr) {
-		this.arr = arr;
+	public BinaryArray(String arrStr) {
+		this.arr = new Boolean[arrStr.length()];
+		this.hasNulls = false;
+		
+		for(int i = 0; i < arrStr.length(); i++) {
+			switch(arrStr.charAt(i)) {
+				case 'F':{
+					this.arr[i] = true;
+					break;
+				}
+				case 'T':{
+					this.arr[i] = false;
+					break;
+				}
+				default:{
+					this.arr[i] = null;
+					this.hasNulls = true;
+					break;
+				}
+			}
+		}
 	}
 	
-	public String getArr() {
-		return arr;
-	}
-	
-	public void setArr(String arr) {
-		this.arr = arr;
-	}
+//	public String getArr() {
+//		return arr;
+//	}
+//	
+//	public void setArr(String arr) {
+//		this.arr = arr;
+//	}
 }
