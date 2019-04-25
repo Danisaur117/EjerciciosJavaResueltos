e36f6758960ca8f9d89671a5202852acd060ba69
Danisaur117 <danisaur@gmail.com>
2019-04-25 17:57:10 +0200

    Module 2 - Activity 2 started but not implemented yet

---
+++ Modulo2Actividad2/src/main/java/com/daniel/belmonte/Modulo2Actividad2/App.java
@@ -0,0 +1,19 @@
+package com.daniel.belmonte.Modulo2Actividad2;
+
+/**
+ * MÓDULO 2 - ACTIVIDAD 2
+ * 
+ * Cree una clase Cuenta que almacena el saldo actual y proporcione los métodos:
+ *  - getBalance: para obtener el saldo
+ *  - deposit: para depositar fondos
+ *  - withdraw: para reitrar fondos
+ *  - toString para devolver por pantalla el saldo actual
+ *  - Al menos un constructor
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
